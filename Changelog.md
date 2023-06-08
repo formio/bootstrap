f0ede0b986a18df612400c13a754a8a9515189bc
Travis Tidwell <travis@form.io>
2023-06-08 15:53:06 -0500

    Updated build.

--- Changelog.md
+++ Changelog.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## 3.0.0-rc.4
+### Fixed
+ - Fixed mjs and cjs template definitions.
+
 ## 3.0.0-rc.2
 ### Changed
  - Added Bootstrap 5 templates
--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const rename = require('gulp-rename');
 const template = require('gulp-template');
 
 // Compile all *.ejs files to pre-compiled templates and append *.js to the filename.
-gulp.task('templates', () =>
+gulp.task('templates-cjs', () =>
   gulp.src('./src/**/*.ejs')
     .pipe(template.precompile({
       evaluate: /\{%([\s\S]+?)%\}/g,
@@ -13,13 +13,26 @@ gulp.task('templates', () =>
       escape: /\{\{\{([\s\S]+?)\}\}\}/g,
       variable: 'ctx'
     }))
-    .pipe(insert.prepend('Object.defineProperty(exports, "__esModule", {\n' +
-      '  value: true\n' +
-      '});\n' +
-      'exports.default='))
+    .pipe(insert.prepend('module.exports='))
+    .pipe(rename({
+      extname: '.ejs.js'
+    }))
+    .pipe(gulp.dest('lib/cjs'))
+);
+
+gulp.task('templates-mjs', () =>
+  gulp.src('./src/**/*.ejs')
+    .pipe(template.precompile({
+      evaluate: /\{%([\s\S]+?)%\}/g,
+      interpolate: /\{\{([\s\S]+?)\}\}/g,
+      escape: /\{\{\{([\s\S]+?)\}\}\}/g,
+      variable: 'ctx'
+    }))
+    .pipe(insert.prepend('export default '))
     .pipe(rename({
       extname: '.ejs.js'
     }))
     .pipe(gulp.dest('lib/mjs'))
-    .pipe(gulp.dest('lib/cjs'))
 );
+
+gulp.task('templates', gulp.parallel(['templates-cjs', 'templates-mjs']));
