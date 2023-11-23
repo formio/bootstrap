63b192a2f6d770433c5feae34deaf7920db10ad4
Travis Tidwell <travis@form.io>
2023-11-23 15:08:50 -0600

    Updated build.

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,14 @@ module.exports = {
     'bootstrap5': './lib/mjs/bootstrap5.js',
   },
   output: {
-    library: '[name]',
+    library: {
+      name: '[name]',
+      type: 'var',
+      export: 'default'
+    },
     libraryTarget: 'umd',
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
+    filename: '[name].js'
   },
   mode: 'production',
   performance: { hints: false },
