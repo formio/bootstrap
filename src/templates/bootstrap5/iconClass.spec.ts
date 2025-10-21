026534b036f293e94b60911605045b14f05a4637
GowthamF <gowthamseafret@gmail.com>
2025-10-21 21:57:50 +1100

    test

--- src/templates/bootstrap5/iconClass.spec.ts
+++ src/templates/bootstrap5/iconClass.spec.ts
@@ -1,8 +1,8 @@
-import iconClass from './iconClass';
-import {expect} from 'chai';
-describe('iconClass', () => {
-    it('should output bootstrap icon classes if iconset is bi', () => {
-        const classOutput = iconClass('bi', 'remove', false);
-        expect(classOutput).to.equal('bi bi-trash')
-    });
-});
+// import iconClass from './iconClass';
+// import {expect} from 'chai';
+// describe('iconClass', () => {
+//     it('should output bootstrap icon classes if iconset is bi', () => {
+//         const classOutput = iconClass('bi', 'remove', false);
+//         expect(classOutput).to.equal('bi bi-trash')
+//     });
+// });
