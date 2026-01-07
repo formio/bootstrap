cc6bd7857613451664833341c8e07eadb3a841ef
kcburge-formio <kcburge-formio@users.noreply.github.com>
2026-01-07 15:23:32 +0000

    $'syncing commit from monorepo. PR: 424, Title: format gulp files'

--- gulpfile.js
+++ gulpfile.js
@@ -6,36 +6,50 @@ const template = require('gulp-template');
 
 // Compile all *.ejs files to pre-compiled templates and append *.js to the filename.
 gulp.task('templates-cjs', () =>
-  gulp.src('./src/**/*.ejs')
-    .pipe(template.precompile({
-      evaluate: /\{%([\s\S]+?)%\}/g,
-      interpolate: /\{\{([\s\S]+?)\}\}/g,
-      escape: /\{\{\{([\s\S]+?)\}\}\}/g,
-      variable: 'ctx'
-    }))
-    .pipe(insert.prepend('Object.defineProperty(exports, "__esModule", {\n' +
-    '  value: true\n' +
-    '});\n' +
-    'exports.default='))
-    .pipe(rename({
-      extname: '.ejs.js'
-    }))
-    .pipe(gulp.dest('lib/cjs'))
+  gulp
+    .src('./src/**/*.ejs')
+    .pipe(
+      template.precompile({
+        evaluate: /\{%([\s\S]+?)%\}/g,
+        interpolate: /\{\{([\s\S]+?)\}\}/g,
+        escape: /\{\{\{([\s\S]+?)\}\}\}/g,
+        variable: 'ctx',
+      }),
+    )
+    .pipe(
+      insert.prepend(
+        'Object.defineProperty(exports, "__esModule", {\n' +
+          '  value: true\n' +
+          '});\n' +
+          'exports.default=',
+      ),
+    )
+    .pipe(
+      rename({
+        extname: '.ejs.js',
+      }),
+    )
+    .pipe(gulp.dest('lib/cjs')),
 );
 
 gulp.task('templates-mjs', () =>
-  gulp.src('./src/**/*.ejs')
-    .pipe(template.precompile({
-      evaluate: /\{%([\s\S]+?)%\}/g,
-      interpolate: /\{\{([\s\S]+?)\}\}/g,
-      escape: /\{\{\{([\s\S]+?)\}\}\}/g,
-      variable: 'ctx'
-    }))
+  gulp
+    .src('./src/**/*.ejs')
+    .pipe(
+      template.precompile({
+        evaluate: /\{%([\s\S]+?)%\}/g,
+        interpolate: /\{\{([\s\S]+?)\}\}/g,
+        escape: /\{\{\{([\s\S]+?)\}\}\}/g,
+        variable: 'ctx',
+      }),
+    )
     .pipe(insert.prepend('export default '))
-    .pipe(rename({
-      extname: '.ejs.js'
-    }))
-    .pipe(gulp.dest('lib/mjs'))
+    .pipe(
+      rename({
+        extname: '.ejs.js',
+      }),
+    )
+    .pipe(gulp.dest('lib/mjs')),
 );
 
 gulp.task('templates', gulp.parallel(['templates-cjs', 'templates-mjs']));
